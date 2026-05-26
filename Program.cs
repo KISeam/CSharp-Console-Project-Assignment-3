afdc6bcd5d48f22e55088147e9981cfc0d56b660
Md. Seam <seam5559@gmail.com>
2026-05-26 21:35:44 +0600

    Create Project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Assignment3
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Your Name: ");
+        string? name = Console.ReadLine();
+
+        Console.Write("Enter Your Age: ");
+        int? age = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Your CGPA: ");
+        double? cgpa = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"CGPA: {cgpa}");
+    }
+}
